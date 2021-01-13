4948a6d781d86a8d630e20bf20eaecca97e1a92d
yhh1056 <yhh1056@naver.com>
2021-01-13 11:35:41 +0900

    Build: project init

---
+++ src/main/java/com/yhh/membercalculator/MemberCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.yhh.membercalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yhh/membercalculator/MemberCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhh.membercalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
