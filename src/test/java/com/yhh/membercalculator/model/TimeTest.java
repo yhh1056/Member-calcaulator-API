394e5e5477a707dbd7835a2e7f9b9268ffa79c10
yhh1056 <yhh1056@naver.com>
2021-01-13 12:44:00 +0900

    Test: create work_time

---
+++ src/test/java/com/yhh/membercalculator/model/TimeTest.java
@@ -0,0 +1,23 @@
+package com.yhh.membercalculator.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author yhh1056
+ * @since 2021/01/13
+ */
+class TimeTest {
+
+    @Test
+    @DisplayName("근무 시간이 생성되는지")
+    void create() {
+        WorkTime workTime = new WorkTime(15, true);
+
+        assertNotNull(workTime);
+        assertEquals(15, workTime.getWorkTime());
+    }
+}
