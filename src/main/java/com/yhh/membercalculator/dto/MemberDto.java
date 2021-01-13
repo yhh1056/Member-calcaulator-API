a1d379a306acc0f64a02070f61961ab988e6aeba
yhh1056 <yhh1056@naver.com>
2021-01-13 16:58:48 +0900

    Test: postMapping(/members) create member

--- src/main/java/com/yhh/membercalculator/dto/MemberDto.java
+++ src/main/java/com/yhh/membercalculator/dto/MemberDto.java
@@ -1,7 +1,9 @@
 package com.yhh.membercalculator.dto;
 
 import com.yhh.membercalculator.model.Member;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -11,10 +13,12 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class MemberDto {
 
     private Long id;
 
+    @NotNull
     private String name;
 
     private TimeDto workWages;
@@ -23,5 +27,9 @@ public class MemberDto {
         this.id = member.getId();
         this.name = member.getName();
         this.workWages = new TimeDto(member.getWorkTimes());
+    }
+
+    public Member toEntity(String name) {
+        return new Member(name);
     }
 }
--- src/test/java/com/yhh/membercalculator/controller/MemberControllerTest.java
+++ src/test/java/com/yhh/membercalculator/controller/MemberControllerTest.java
@@ -1,12 +1,13 @@
 package com.yhh.membercalculator.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.refEq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yhh.membercalculator.dto.MemberDto;
 import com.yhh.membercalculator.model.Member;
 import com.yhh.membercalculator.model.WorkTime;
@@ -14,20 +15,23 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
  * @author yhh1056
  * @since 2021/01/13
  */
-
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(MemberController.class)
 class MemberControllerTest {
 
@@ -36,6 +40,9 @@ class MemberControllerTest {
 
     @MockBean
     private MemberController memberController;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     @DisplayName("/members json형태로 반환되는지")
@@ -52,7 +59,8 @@ class MemberControllerTest {
         List<MemberDto> members = new ArrayList<>();
         members.add(new MemberDto(member));
 
-        when(memberController.getMembers()).thenReturn(new ResponseEntity<>(members, HttpStatus.OK));
+        when(memberController.getMembers())
+            .thenReturn(new ResponseEntity<>(members, HttpStatus.OK));
 
         mockMvc.perform(get("/members"))
             .andExpect(status().isOk())
@@ -60,5 +68,22 @@ class MemberControllerTest {
                 + "\"name\":\"tester\","
                 + "\"workWages\":{\"times\":[104640,104640,104640,104640],"
                 + "\"totalWage\":418560}}]"));
+    }
+
+    @Test
+    @DisplayName("/members 멤버 데이터를 이용하여 멤버 생성")
+    void createMember() throws Exception {
+        MemberDto memberDto = new MemberDto();
+        memberDto.setName("tester");
+
+        when(memberController.addMember(memberDto)).thenReturn(new ResponseEntity<>(HttpStatus.CREATED
+        ));
+
+        mockMvc.perform(post("/members")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(memberDto)))
+            .andExpect(status().isOk())
+            .andDo(print());
     }
 }
