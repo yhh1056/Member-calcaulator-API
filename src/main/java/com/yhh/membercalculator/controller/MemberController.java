2a61f90b75b162dec235e7111918b39cc393e48e
yhh1056 <yhh1056@naver.com>
2021-01-13 18:44:41 +0900

    Feat: getMapping(/member/{id}) get member

--- src/main/java/com/yhh/membercalculator/controller/MemberController.java
+++ src/main/java/com/yhh/membercalculator/controller/MemberController.java
@@ -10,8 +10,10 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -38,5 +40,14 @@ public class MemberController {
         Member member = nameDto.toEntity();
 
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @ApiOperation(value = "id로 회원 조회", notes = "id로 회원을 조회합니다.")
+    @GetMapping("/members/{id}")
+    public ResponseEntity<Member> getMember(@PathVariable("id") Long id) {
+        /**
+         * repository에서 회원 가져오기
+         */
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
