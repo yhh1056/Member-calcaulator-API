75dded2f1e90ead6669213d5cf00b49661bf9f76
yhh1056 <yhh1056@naver.com>
2021-01-31 13:38:29 +0900

    Feat: add swagger info

--- src/main/java/com/yhh/membercalculator/config/SwaggerConfig.java
+++ src/main/java/com/yhh/membercalculator/config/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.yhh.membercalculator.config;
 
+import java.util.ArrayList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,9 +23,23 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
             .select()
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.any())
             .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+            "title",
+            "description",
+            "version",
+            "uri",
+            new Contact("Contact Me", "url", "@mail"),
+            "라이센스",
+            "라이센스",
+            new ArrayList<>()
+        );
     }
 }
